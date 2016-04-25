2c66149d78bff7e05ef23c98878751c640681651
ZPaulo <jose.p.s.olive@gmail.com>
2016-04-25 02:23:21 +0100

    Android a funcar

--- Smallet/Smallet.Droid/MainActivity.cs
+++ Smallet/Smallet.Droid/MainActivity.cs
@@ -5,13 +5,55 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Locations;
+using System.Collections.Generic;
+using Android.Util;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
 
 namespace Smallet.Droid
 {
     [Activity(Label = "Smallet.Droid", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    public class MainActivity : Activity, ILocationListener
     {
-        int count = 1;
+        static readonly string TAG = "X:" + typeof(MainActivity).Name;
+        TextView addressText;
+        Location currentLocation;
+        LocationManager locationManager;
+
+        string locationProvider;
+        TextView locationText;
+
+        public async void OnLocationChanged(Location location)
+        {
+            currentLocation = location;
+            if (currentLocation == null)
+            {
+                locationText.Text = "Unable to determine your location. Try again in a short while.";
+            }
+            else
+            {
+                locationText.Text = string.Format("{0:f6},{1:f6}", currentLocation.Latitude, currentLocation.Longitude);
+                Address address = await ReverseGeocodeCurrentLocation();
+                DisplayAddress(address);
+            }
+        }
+
+        public void OnProviderDisabled(string provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnProviderEnabled(string provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnStatusChanged(string provider, [GeneratedEnum] Availability status, Bundle extras)
+        {
+            throw new NotImplementedException();
+        }
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -20,12 +62,81 @@ namespace Smallet.Droid
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            addressText = FindViewById<TextView>(Resource.Id.address_text);
+            locationText = FindViewById<TextView>(Resource.Id.location_text);
+            FindViewById<TextView>(Resource.Id.get_address_button).Click += AddressButton_OnClick;
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            InitializeLocationManager();
         }
+
+        private void InitializeLocationManager()
+        {
+            locationManager = (LocationManager)GetSystemService(LocationService);
+            Criteria criteriaForLocationService = new Criteria
+            {
+                Accuracy = Accuracy.Fine
+            };
+            IList<string> acceptableLocationProviders = locationManager.GetProviders(criteriaForLocationService, true);
+
+            if (acceptableLocationProviders.Any())
+            {
+                locationProvider = acceptableLocationProviders.First();
+            }
+            else
+            {
+                locationProvider = string.Empty;
+            }
+            Log.Debug(TAG, "Using " + locationProvider + ".");
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            locationManager.RequestLocationUpdates(locationProvider, 2000, 1, this);
+        }
+
+        async void AddressButton_OnClick(object sender, EventArgs eventArgs)
+        {
+            if (currentLocation == null)
+            {
+               addressText.Text = "Can't determine the current address. Try again in a few minutes.";
+                return;
+            }
+
+            Address address = await ReverseGeocodeCurrentLocation();
+            DisplayAddress(address);
+        }
+
+        async Task<Address> ReverseGeocodeCurrentLocation()
+        {
+            Geocoder geocoder = new Geocoder(this);
+            IList<Address> addressList =
+                await geocoder.GetFromLocationAsync(currentLocation.Latitude, currentLocation.Longitude, 10);
+
+            Address address = addressList.FirstOrDefault();
+            return address;
+        }
+
+        void DisplayAddress(Address address)
+        {
+            if (address != null)
+            {
+                StringBuilder deviceAddress = new StringBuilder();
+                for (int i = 0; i < address.MaxAddressLineIndex; i++)
+                {
+                    deviceAddress.AppendLine(address.GetAddressLine(i));
+                }
+                // Remove the last comma from the end of the address.
+                addressText.Text = deviceAddress.ToString();
+            }
+            else
+            {
+                addressText.Text = "Unable to determine the address. Try again in a few minutes.";
+            }
+        }
+
+
+
     }
 }
 
